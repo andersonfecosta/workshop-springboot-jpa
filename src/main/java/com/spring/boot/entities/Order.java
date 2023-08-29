45812cf55dcaf2218e559cac08b5444388a8a081
Anderson Costa <andersonfelipedc@gmail.com>
2023-08-29 18:47:16 -0300

    OrderStatus enum

--- src/main/java/com/spring/boot/entities/Order.java
+++ src/main/java/com/spring/boot/entities/Order.java
@@ -1,6 +1,7 @@
 package com.spring.boot.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.spring.boot.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,7 +21,25 @@ public class Order {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 }
